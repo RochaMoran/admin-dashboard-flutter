b7feb20cdc69d5b91263072827dbd159b447651b
RochaMoran <rochamoran1@gmail.com>
2024-12-14 16:15:36 -0600

    feat: login and register validations

--- lib/ui/views/register_view.dart
+++ lib/ui/views/register_view.dart
@@ -1,73 +1,102 @@
+import 'package:admin_dashboard/providers/register_form_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
 import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegisterView extends StatelessWidget {
   const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.only(top: 100),
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 370),
-            child: Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    // validator: ,
-                    style: const TextStyle(
-                      color: Colors.white,
+    return ChangeNotifierProvider(
+      create: (_) => RegisterFormProvider(),
+      child: Builder(builder: (context) {
+        final registerFormProvider = Provider.of<RegisterFormProvider>(context);
+
+        return Container(
+          margin: const EdgeInsets.only(top: 100),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 370),
+              child: Form(
+                autovalidateMode: AutovalidateMode.always,
+                key: registerFormProvider.formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value){
+                        if(value == null || value.isEmpty) return 'Ingrese su nombre';
+                        if(value.length < 3) return 'El nombre debe contener almenos 3 caracteres';
+                        return null;
+                      },
+                      onChanged: (value) => registerFormProvider.name = value,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      cursorColor: Colors.white,
+                      decoration: CustomInputs.authInputDecoration(
+                          hint: 'Ingrese su nombre',
+                          label: 'Nombre',
+                          icon: Icons.person),
                     ),
-                    cursorColor: Colors.white,
-                    decoration: CustomInputs.authInputDecoration(
-                        hint: 'Ingrese su nombre',
-                        label: 'Nombre',
-                        icon: Icons.person),
-                  ),
-                      const SizedBox(height: 20),
-                  TextFormField(
-                    // validator: ,
-                    style: const TextStyle(
-                      color: Colors.white,
+                        const SizedBox(height: 20),
+                    TextFormField(
+                      onChanged: (value) => registerFormProvider.email = value,
+                      validator: (value) {
+                        if(EmailValidator.validate(value!)) return null;
+                        
+                        return 'Ingrese un correo valido';
+                      },
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      cursorColor: Colors.white,
+                      decoration: CustomInputs.authInputDecoration(
+                          hint: 'Ingrese su correo',
+                          label: 'Correo',
+                          icon: Icons.email),
                     ),
-                    cursorColor: Colors.white,
-                    decoration: CustomInputs.authInputDecoration(
-                        hint: 'Ingrese su correo',
-                        label: 'Correo',
-                        icon: Icons.email),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    // validator: ,
-                    obscureText: true,
-                    style: const TextStyle(
-                      color: Colors.white,
+                    const SizedBox(height: 20),
+                    TextFormField(
+                       validator: (value){
+                        if(value == null || value.isEmpty) return 'Ingrese su contraseña';
+                        if(value.length < 6) return 'La contraseña debe contener almenos 6 caracteres';
+                        return null;
+                      },
+                      onChanged: (value) => registerFormProvider.password = value,
+                      obscureText: true,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      cursorColor: Colors.white,
+                      decoration: CustomInputs.authInputDecoration(
+                          hint: '**********',
+                          label: 'Contraseña',
+                          icon: Icons.lock),
                     ),
-                    cursorColor: Colors.white,
-                    decoration: CustomInputs.authInputDecoration(
-                        hint: '**********',
-                        label: 'Contraseña',
-                        icon: Icons.lock),
-                  ),
-                  const SizedBox(height: 20),
-                  CustomOutlinedButton(
-                      onPressed: () {}, text: "Crear Cuenta"),
-                  const SizedBox(height: 20),
-                  LinkText(
-                      text: 'Ya tengo una cuenta',
-                      onPressed: () {
-                           Navigator.pushNamed(context, Flurorouter.loginRoute);
-                      })
-                ],
+                    const SizedBox(height: 20),
+                    CustomOutlinedButton(
+                        onPressed: () {
+                          registerFormProvider.validateForm();
+                        }, text: "Crear Cuenta"),
+                    const SizedBox(height: 20),
+                    LinkText(
+                        text: 'Ya tengo una cuenta',
+                        onPressed: () {
+                             Navigator.pushNamed(context, Flurorouter.loginRoute);
+                        })
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          ));
+      })
+    );
   }
 
   
